f6f42e3a924f790be16b13269692148ecb81d50b
rsmkhal <133159514+rsmkhal@users.noreply.github.com>
2025-08-22 20:31:27 -0400

    hashrouting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Recipes from './Recipes';
 import About from './About';
